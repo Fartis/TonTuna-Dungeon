e100a05cc15657701f73bf8e96ce5b795a28e5e6
farti <farti@DESKTOP-17F31JV>
2017-04-19 12:58:56 +0200

    Finalizados los JFrame de: 	Menu principal 	Menu nueva partida 	Mensaje de inicio de partida
    Añadido metodos de inicialización de personaje.
    Añadidos fondos animados en frames.

---
+++ TonTunaDungeon/src/Vista/Elementos/VentanaMazmorra.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista.Elementos;
+
+import Controlador.ControladorGUI;
+import Vista.JFramePrincipal;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Manuel David Villalba Escamilla
+ */
+public class VentanaMazmorra extends JPanel {
+
+    private Image image = Toolkit.getDefaultToolkit().getImage("src/Recursos/dungeon.gif");
+    JFramePrincipal padre;
+
+    public VentanaMazmorra() {
+    }
+    
+    public void setPadre(JFramePrincipal padre) {
+        this.padre = padre;
+    }
+
+    private void jButtonIniciarActionPerformed(java.awt.event.ActionEvent evt){
+        // TODO add your handling code here:
+        ControladorGUI.getSingleton().menuNuevaPartida();
+        this.setVisible(false);
+        padre.dispose();
+        
+        
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, 800, 600, this);
+    }
+}
