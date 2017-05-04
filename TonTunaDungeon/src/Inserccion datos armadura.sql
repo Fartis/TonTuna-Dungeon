dc4ad17258dc88670215937e232a7eab93faf2ce
SrCharles <SrCharles@DESKTOP-G3N9HBV>
2017-05-04 14:25:42 +0200

    javadoc de ventanas

---
+++ TonTunaDungeon/src/Inserccion datos armadura.sql
@@ -0,0 +1,5 @@
+insert into armadura (nombre, bonificador, indice, descripcion) values
+	("Armadura de cartón-piedra", 1, 3,"Con ella se mueve uno acartonao pero menos da una piedra"),
+	("Armadura Acolchada", 1, 5,"Armadura de tela acolchada, fácil de romper pero algo quita"),
+	("Cuero", 2, 6,"Armadura de cuero, cómo una segunda piel"),
+	("Cuero endurecido", 2, 8, "Armadura de cuero endurecido que resiste más"),
