f2f2e177e41117f32c694f80742b2af3df7ab196
SrCharles <SrCharles@DESKTOP-G3N9HBV>
2017-05-05 09:03:11 +0200

    javadoc de ventanas

--- TonTunaDungeon/src/Inserccion datos armadura.sql
+++ TonTunaDungeon/src/Inserccion datos armadura.sql
@@ -3,3 +3,9 @@ insert into armadura (nombre, bonificador, indice, descripcion) values
 	("Armadura Acolchada", 1, 5,"Armadura de tela acolchada, fácil de romper pero algo quita",1),
 	("Cuero", 2, 6,"Armadura de cuero, cómo una segunda piel",2),
 	("Cuero endurecido", 2, 8, "Armadura de cuero endurecido que resiste más",2),
+	("Túnica mágica", 3, 7, "Túnica con un hechizo que resiste los golpes, aunque la tendrás que reparar igualmente",3),
+	("Cuero Tachonado", 3, 9, "Armadura de cuero con tachuelas que aguantan algunos golpes",3),
+	("Armadura de RoscaChapa", 4, 10, "Hecha de placas de chapa que truenan al ser golpeada pero te protegerá",4),
+	("Cota de malla", 4, 12, "Igual que un jersey tricotado pero de anillas de hierro",4),
+	("Cota de escamas", 5, 15, "Fabricada en escamas metálicas que protegen a la vez que brillan",5),
+	("Armadura completa", 5, 20, "Coraza que cubre todo el cuerpo fabricada con hierro",5);
